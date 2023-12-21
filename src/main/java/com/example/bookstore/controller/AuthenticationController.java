c2217a391d7aeee505491caa774617234ad955fc
Roman Menzheres <roma.menzheres@gmail.com>
2023-12-21 17:45:54 +0200

    Style fixes

--- src/main/java/com/example/bookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/bookstore/controller/AuthenticationController.java
@@ -3,7 +3,6 @@ package com.example.bookstore.controller;
 import com.example.bookstore.dto.UserDto;
 import com.example.bookstore.dto.UserRegistrationRequestDto;
 import com.example.bookstore.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
