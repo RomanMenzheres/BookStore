be8bb080d12084a94223b64e5f47fbd8fadf76ae
Roman Menzheres <roma.menzheres@gmail.com>
2023-12-28 22:11:01 +0200

    Fixed bug with empty set

--- src/main/java/com/example/bookstore/model/ShoppingCart.java
+++ src/main/java/com/example/bookstore/model/ShoppingCart.java
@@ -13,8 +13,9 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.SQLDelete;
@@ -25,7 +26,8 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("is_deleted = false")
 @Table(name = "shopping_carts")
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
